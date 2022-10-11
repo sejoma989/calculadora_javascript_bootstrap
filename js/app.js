d0b6185c39e6c8b02613d6620f4a252db88decde
jose_pro <sejoma989@gmail.com>
2022-10-11 18:59:24 -0500

    feat: buttons functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,45 @@
 console.log('Aplicacion calculadora');
 
-function sumar(operandoA, operandoB){
-    
+const forma = document.getElementById('forma');
+let operandoA = forma['operandoA'];
+let operandoB = forma['operandoB'];
+
+
+function sumar(){
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if (isNaN(resultado)){
+        resultado = 'Por favor ingrese numeros'
+    }
+    document.getElementById('resul').innerHTML = `Resultado de la suma: ${resultado}`;
+    console.log(`Resultado de la suma: ${resultado}`);
+}
+
+function restar(){
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if (isNaN(resultado)){
+        resultado = 'Por favor ingrese numeros'
+    }
+    document.getElementById('resul').innerHTML = `Resultado de la resta: ${resultado}`;
+    console.log(`Resultado de la resta: ${resultado}`);
+
+}
+
+function multiplicar(){
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if (isNaN(resultado)){
+        resultado = 'Por favor ingrese numeros'
+    }
+    document.getElementById('resul').innerHTML = `Resultado de la multiplicacion: ${resultado}`;
+    console.log(`Resultado de la multiplicacion: ${resultado}`);
+
+}
+
+function dividir(){
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if (isNaN(resultado)){
+        resultado = 'Por favor ingrese numeros'
+    }
+    document.getElementById('resul').innerHTML = `Resultado de la division: ${resultado}`;
+    console.log(`Resultado de la division: ${resultado}`);
+
 }
